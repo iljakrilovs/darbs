b036819720ee0ea1b16ce06dc5ec65526a314032
iljakrilovs <ilja.prog@gmail.com>
2025-05-22 20:29:16 +0300

    mēgināju

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const cenas = {
   day: 5.0,
   month: 30.0
 }
+
+const braucieniInput = document.querySelector(".braucieni_input");
+const bileteSelect = document.querySelector(".bilete_select");
+const rezultats = document.querySelector(".result");
+
+function ievaditBraucienuSkaitu() {
+ }
+function izveletiesBiletesVeidu() {
+ }
+function aprekinatIzmaksas() {
+};
